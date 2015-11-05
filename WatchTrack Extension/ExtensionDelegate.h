56e748b9eddfa2919e602314b2b7a559d26e6fad
Jason Ji <uberjason@gmail.com>
2015-11-04 21:44:15 -0500

    Added Watch app target to project

---
+++ WatchTrack Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  WatchTrack Extension
+//
+//  Created by Jason Ji on 11/4/15.
+//  Copyright © 2015 Jason Ji. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ WatchTrack Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  WatchTrack Extension
+//
+//  Created by Jason Ji on 11/4/15.
+//  Copyright © 2015 Jason Ji. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
