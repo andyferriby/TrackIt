8bb61637d217a24e2a88f4de69125f7ec8410252
Jason Ji <uberjason@gmail.com>
2017-09-13 22:17:07 -0400

    Update some constraints to work for iPhone X. Update WatchSessionDelegate with additional empty delegate methods to suppress warning.

--- TrackIt/Models/WatchSessionDelegate.m
+++ TrackIt/Models/WatchSessionDelegate.m
@@ -70,4 +70,19 @@
     }
 }
 
+- (void)session:(nonnull WCSession *)session activationDidCompleteWithState:(WCSessionActivationState)activationState error:(nullable NSError *)error { 
+    
+}
+
+
+- (void)sessionDidBecomeInactive:(nonnull WCSession *)session { 
+    
+}
+
+
+- (void)sessionDidDeactivate:(nonnull WCSession *)session { 
+    
+}
+
+
 @end
